246f67d4288df484e12df649c7b1b1eec794f5cd
cecep31 <cecepjanuardi31@gmail.com>
2023-12-25 15:13:49 +0700

    Update posts controller to return status code and headers

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -3,7 +3,10 @@ import { Elysia } from "elysia";
 export const posts = new Elysia();
 
 posts.group("/posts", (posts) => (
-    posts.get("/", () => "Hello Elysia posts")
+    posts.get("/", (ctx) => {
+        ctx.set.status = 200
+        return ctx.headers
+    })
         .get("/:id", () => "Hello Elysia posts")
         .post("/", () => "Hello Elysia posts")
         .put("/:id", () => "Hello Elysia posts")
